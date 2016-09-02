7d167ce975c9ed97c74fe302403eece0cf217c6a
Freud Kang <luoyan35714@126.com>
2016-09-02 17:51:58 +0800

    change the left tree sub nav to show normal

--- styles/js/lessismore.js
+++ styles/js/lessismore.js
@@ -7,6 +7,7 @@
 
   $(function () {
       $('#navigation .sidenav').html($("#markdown-toc").html());
+      $('#navigation .sidenav ul').addClass("nav");
       $("#markdown-toc").remove();
   })
 
